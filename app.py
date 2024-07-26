41ec3b759a3b6b39794e2960f6bb990d70a8930d
Bernhard-Keller123 <bkeller1@gwdg.de>
2024-07-26 18:58:55 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import json
 import chardet
 
 # Greife auf den API-Schlüssel aus der Umgebungsvariable zu
-api_key = st.secrets['OPENAI_API']
-
+#api_key = st.secrets['OPENAI_API']
+api_key = "sk-proj-hPGEYtUGV9ljosiIjjgVT3BlbkFJUI6BpHRh86xKd1Z01cTc"
 
 
 
